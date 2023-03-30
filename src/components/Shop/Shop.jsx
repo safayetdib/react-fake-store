30516edee976f284adaca153b3e05dd27ad731e8
safayet <safayetdib@gmail.com>
2023-03-30 19:47:55 +0600

    added data loading from local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDb } from '../../utilities/utilities';
+import { addToDb, getShoppingCart } from '../../utilities/utilities';
 
 const Shop = () => {
 	const [products, setProducts] = useState([]);
@@ -13,20 +13,39 @@ const Shop = () => {
 			.then((data) => setProducts(data));
 	}, []);
 
+	useEffect(() => {
+		const storedCart = getShoppingCart();
+		const savedCart = [];
+
+		for (const id in storedCart) {
+			const addedProduct = products.find((product) => {
+				// console.log(product.id, id);
+				return product.id.toString() === id;
+			});
+			if (addedProduct) {
+				const quantity = storedCart[id];
+				// console.log(quantity);
+				addedProduct.quantity = quantity;
+				savedCart.push(addedProduct);
+			}
+		}
+		setCart(savedCart);
+	}, [products]);
+
 	const handleAddToCart = (product) => {
 		let newCart = [];
-
+		// find previous data in cart
 		const exists = cart.find((pd) => pd.id === product.id);
-
 		if (!exists) {
+			// if there is no prev data then set quantity 1 and update cart
 			product.quantity = 1;
 			newCart = [...cart, product];
 		} else {
+			// if prev data exists then update quantity and cart
 			exists.quantity = exists.quantity + 1;
 			const remaining = cart.filter((pd) => pd.id !== product.id);
 			newCart = [...remaining, exists];
 		}
-
 		setCart(newCart);
 		addToDb(product.id);
 	};
